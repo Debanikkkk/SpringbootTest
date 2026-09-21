9fac7f26af01bf0884e342ad73cd9c0129b7d834
Debanik Pahilwani <debanik@Debaniks-MacBook-Pro.local>
2026-09-21 08:15:45 +0530

    basic sp project

---
+++ src/main/java/com/debanikspring/testprojectspring/TestprojectspringApplication.java
@@ -0,0 +1,13 @@
+package com.debanikspring.testprojectspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestprojectspringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestprojectspringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/debanikspring/testprojectspring/TestprojectspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.debanikspring.testprojectspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestprojectspringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
